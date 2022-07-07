e212e4f1cee662a50c41a49d6f5b1bdfcb2be0f6
Keval Rana <keval.rana@mindinventory.com>
2022-07-07 18:43:26 +0530

    update

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ app.post('/login',async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port ${port}`)
 })
